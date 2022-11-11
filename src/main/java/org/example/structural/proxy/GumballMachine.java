5dbdaed1890bb793027c0d2e0e9a94b55ec28e65
ddold <ddold@cloudbees.com>
2022-11-11 08:19:48 +0000

    proxy patterns

---
+++ src/main/java/org/example/structural/proxy/GumballMachine.java
@@ -0,0 +1,111 @@
+package org.example.structural.proxy;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class GumballMachine extends UnicastRemoteObject implements GumballMachineRemote {
+
+    private static final long serialVersionUID = 2L;
+    State soldOutState;
+    State noQuarterState;
+    State hasQuarterState;
+    State soldState;
+    State winnerState;
+
+    State state;
+    int count = 0;
+    String location;
+
+    public GumballMachine(String location, int numGumballs) throws RemoteException {
+        this.soldOutState = new SoldOutState(this);
+        this.noQuarterState = new NoQuarterState(this);
+        this.hasQuarterState = new HasQuarterState(this);
+        this.soldState = new SoldState(this);
+        this.winnerState = new WinnerState(this);
+
+        this.location = location;
+        this.count = numGumballs;
+        if (numGumballs > 0) {
+            state = noQuarterState;
+        } else {
+            state = soldState;
+        }
+    }
+
+    public void insertQuarter() {
+        state.insertQuarter();
+    }
+
+    public void ejectQuarter() {
+        state.ejectQuarter();
+    }
+
+    public void turnCrank() {
+        state.turnCrank();
+        state.dispense();
+    }
+
+    void setState(State state) {
+        this.state = state;
+    }
+
+    void releaseBall() {
+        System.out.println("A gumball comes rolling out the slot...");
+        if (count != 0) {
+            count -= 1;
+        }
+    }
+
+    public State getSoldOutState() {
+        return soldOutState;
+    }
+
+    public State getNoQuarterState() {
+        return noQuarterState;
+    }
+
+    public State getHasQuarterState() {
+        return hasQuarterState;
+    }
+
+    public State getSoldState() {
+        return soldState;
+    }
+
+    public State getWinnerState() {
+        return winnerState;
+    }
+
+    @Override
+    public State getState() {
+        return state;
+    }
+
+    @Override
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String getLocation() {
+        return location;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("\nMighty Gumball, Inc.");
+        result.append("\nJava-enabled Standing Gumball Model #2004");
+        result.append("\nInventory: ");
+        result.append(count);
+        result.append(" gumball");
+        if (count != 1) {
+            result.append("s");
+        }
+        result.append("\n");
+        result.append("Machine is ");
+        result.append(state);
+        result.append("\n");
+        return result.toString();
+    }
+}
---
+++ src/main/java/org/example/structural/proxy/State.java
@@ -0,0 +1,10 @@
+package org.example.structural.proxy;
+
+import java.io.Serializable;
+
+public interface State extends Serializable {
+    void insertQuarter();
+    void ejectQuarter();
+    void turnCrank();
+    void dispense();
+}
